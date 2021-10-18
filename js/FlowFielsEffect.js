dd7955f977ebc8d0899394420f5d478ef7c353a3
c-yator <christian.yator@students.jkuat.ac.ke>
2021-10-18 21:48:29 +0300

    animate, delta time sync and grid

--- js/FlowFielsEffect.js
+++ js/FlowFielsEffect.js
@@ -2,18 +2,39 @@ export default class FlowFieldEffect {
 	#ctx;
 	#width;
 	#height;
-	constructor(ctx, width, height) {
+	constructor(ctx, width, height, mouse) {
 		this.#ctx = ctx;
 		this.#ctx.strokeStyle = 'white';
+		this.#ctx.lineWidth = 5;
 		this.#width = width;
 		this.#height = height;
-		this.#draw(10, 10);
+		this.mouse = mouse;
+		this.lastTime = 0;
+		this.interval = 1000 / 60; //60fps
+		this.timer = 0;
+		this.cellSize = 50;
 	}
-	#draw(x, y) {
-		const length = 200;
+	#drawLine(x, y) {
 		this.#ctx.beginPath();
 		this.#ctx.moveTo(x, y);
-		this.#ctx.lineTo(x + length, y + length);
+		this.#ctx.lineTo(x + 25, y + 10);
 		this.#ctx.stroke();
 	}
+	animate = (timeStamp) => {
+		const deltaTime = timeStamp - this.lastTime;
+		this.lastTime = timeStamp;
+		if (this.timer > this.interval) {
+			this.#ctx.clearRect(0, 0, this.#width, this.#height);
+			for (let y = 0; y < this.#height; y += this.cellSize) {
+				for (let x = 0; x < this.#width; x += this.cellSize) {
+					this.#drawLine(x, y);
+				}
+			}
+			this.timer = 0;
+		} else {
+			this.timer += deltaTime;
+		}
+		let flowFieldAnimation = requestAnimationFrame(this.animate);
+		return flowFieldAnimation;
+	};
 }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
 import FlowFielsEffect from './FlowFielsEffect.js';
+
+let canvas;
+let ctx;
+let flowField;
+let flowFieldAnimation;
+const mouse = {
+	x: 0,
+	y: 0,
+};
+
 window.onload = () => {
-	const canvas = document.getElementById('my-canvas');
-	const ctx = canvas.getContext('2d');
+	canvas = document.getElementById('my-canvas');
+	ctx = canvas.getContext('2d');
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	const flowField = new FlowFielsEffect(ctx, canvas.width, canvas.height);
+	flowField = new FlowFielsEffect(ctx, canvas.width, canvas.height, mouse);
+	flowFieldAnimation = flowField.animate(0);
 };
+
+window.addEventListener('resize', () => {
+	cancelAnimationFrame(flowFieldAnimation);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	flowField = new FlowFielsEffect(ctx, canvas.width, canvas.height);
+	flowField.animate(0);
+});
+
+window.addEventListener('mousemove', (e) => {
+	mouse.x = e.x;
+	mouse.y = e.y;
+});
